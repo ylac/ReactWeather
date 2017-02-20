2481766cf8c741d3758cfc2160e69876b55a861b
Caly <caly@inventa.com.au>
2017-02-20 17:44:00 +1100

    Update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,16 +1,8 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>Examples Component</h3>
-//     )
-//   }
-// });
-
 var Examples = (props) => {
   return (
-    <h3>Examples Component</h3>
+    <h3>Examples Component!</h3>
   )
 }
 
