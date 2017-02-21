4bf87de7e1ecf3332c6037bbf9bd5f2855d98d05
Caly <caly@inventa.com.au>
2017-02-21 13:25:31 +1100

    Style about page and link resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,12 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className='text-center'>About</h1>
+      <p>This is a basic weather app built on
+        <a href="https://facebook.github.io/react"> React</a>.
+        It uses weather data from
+        <a href='http://openweathermap.org/'> OpenWeatherMap </a>
+        and the source code is stored on
+        <a href='https://github.com/ylac/ReactWeather'> Github</a>.
+      </p>
     </div>
-  )
+  );
 }
 
 module.exports = About;
