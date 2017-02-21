e66b0af8d62a7a331b9283a2bb91e2c3bf241682
Caly <caly@inventa.com.au>
2017-02-21 12:46:45 +1100

    Add columns and update examples.

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out.</p>
+      <ol>
+        <li>
+          <Link to='/?location=sydney'>Sydney, NSW</Link>
+        </li>
+        <li>
+          <Link to='/?location=beijing'>Beijing</Link>
+        </li>
+      </ol>
     </div>
   )
 }
