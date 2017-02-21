44ab70760abd78ae932d203ce2cb46dde560df9c
Caly <caly@inventa.com.au>
2017-02-21 13:32:33 +1100

    Add styles to weather page

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -16,7 +16,7 @@ var WeatherForm = React.createClass({
       <div>
         <form onSubmit={this.onFormSubmit}>
           <input type="text" ref="location" placeholder="Enter city name"></input>
-          <button>Get Weather</button>
+          <button className="button expanded hollow">Get Weather</button>
         </form>
       </div>
     )
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({location, temp}) => {
     return (
-      <h4>It is {temp} degrees Celsius in {location}.</h4>
+      <h4 className="text-center">It is {temp} degrees Celsius in {location}.</h4>
     )
 };
 
